d3b33efddcf9670eabab668a55921211d823b48d
superior_shrimp <szymon.zychowicz300701@gmail.com>
2022-03-25 13:24:28 +0100

    lab start

---
+++ lab4/src/Board.java
@@ -0,0 +1,212 @@
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+
+public class Board extends JComponent implements MouseInputListener, ComponentListener {
+    private static final long serialVersionUID = 1L;
+    private Point[][] points;
+    private int size = 25;
+    public int editType = 0;
+
+    public Board(int length, int height) {
+        addMouseListener(this);
+        addComponentListener(this);
+        addMouseMotionListener(this);
+        setBackground(Color.WHITE);
+        setOpaque(true);
+    }
+
+    private void initialize(int length, int height) {
+        points = new Point[length][height];
+        int len;
+        if(points != null){
+            len = points.length;
+        }
+        else{
+            len = 0;
+        }
+        for (int x = 0; x < len; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y] = new Point();
+                if(y != 2 && y != 3){
+                    points[x][y].type = 5;
+                }
+                else{
+                    points[x][y].type = 0;
+                }
+            }
+        }
+        for (int x = 0; x < len; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                if(x + 1 < points.length){
+                    points[x][y].next = points[x + 1][y];
+                }
+                else{
+                    points[x][y].next = points[0][y];
+                }
+            }
+        }
+    }
+
+    public void iteration() {
+        int len;
+        if(points != null){
+            len = points.length;
+        }
+        else{
+            len = 0;
+        }
+
+        for (int x = 0; x < len; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y].moved = false;
+            }
+        }
+
+        for (int x = 0; x < len; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                if(!points[x][y].moved){
+                    points[x][y].move();
+                }
+            }
+        }
+        this.repaint();
+    }
+
+    public void clear() {
+        int len;
+        if(points!=null){
+            len = points.length;
+        }
+        else{
+            len = 0;
+        }
+        for (int x = 0; x < len; ++x)
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y].clear();
+            }
+        this.repaint();
+    }
+
+
+    protected void paintComponent(Graphics g) {
+        if (isOpaque()) {
+            g.setColor(getBackground());
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        g.setColor(Color.GRAY);
+        drawNetting(g, size);
+    }
+
+    private void drawNetting(Graphics g, int gridSpace) {
+        Insets insets = getInsets();
+        int firstX = insets.left;
+        int firstY = insets.top;
+        int lastX = this.getWidth() - insets.right;
+        int lastY = this.getHeight() - insets.bottom;
+
+        int x = firstX;
+        while (x < lastX) {
+            g.drawLine(x, firstY, x, lastY);
+            x += gridSpace;
+        }
+
+        int y = firstY;
+        while (y < lastY) {
+            g.drawLine(firstX, y, lastX, y);
+            y += gridSpace;
+        }
+        int len;
+        if(points!=null){
+            len = points.length;
+        }
+        else{
+            len = 0;
+        }
+        for (x = 0; x < len; ++x) {
+            for (y = 0; y < points[x].length; ++y) {
+                if(points[x][y].type == 0){
+                    g.setColor(Color.WHITE);
+                }
+                else if(points[x][y].type == 1){
+                    g.setColor(Color.YELLOW);
+                }
+                else if(points[x][y].type == 2){
+                    g.setColor(Color.BLUE);
+                }
+                else if(points[x][y].type == 3){
+                    g.setColor(Color.RED);
+                }
+                else if(points[x][y].type == 5){
+                    g.setColor(Color.GREEN);
+                }
+
+                g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
+            }
+        }
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if(editType==0){
+                points[x][y].clicked();
+            }
+            else {
+                points[x][y].type = editType;
+            }
+            this.repaint();
+        }
+    }
+
+    public void componentResized(ComponentEvent e) {
+        int dlugosc = (this.getWidth() / size) + 1;
+        int wysokosc = (this.getHeight() / size) + 1;
+        initialize(dlugosc, wysokosc);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if(editType==0){
+                points[x][y].clicked();
+            }
+            else {
+                points[x][y].type= editType;
+            }
+            this.repaint();
+        }
+    }
+
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void componentShown(ComponentEvent e) {
+    }
+
+    public void componentMoved(ComponentEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void componentHidden(ComponentEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+}
---
+++ lab4/src/Point.java
@@ -0,0 +1,55 @@
+
+public class Point {
+    public int type;
+    public Point next;
+    public boolean moved;
+    public int speed;
+    public static Integer[] types = {0, 1, 2, 3, 5};
+
+    private int distance(){
+        int counter = 0;
+        int maxSpeed = 1 + 2*this.type;
+        Point car = this.next;
+        while(counter < maxSpeed && counter < this.speed + 1 && !car.moved){
+            if(car.type != 0){
+                return counter;
+            }
+            counter++;
+            car.moved = true;
+            car = car.next;
+        }
+        return counter;
+    }
+
+    public void move() {
+        if(this.type == 0 || this.type == 5){return;}
+        this.moved = true;
+        int dist = this.distance();
+        Point car = this.next;
+        if(dist == 0){
+            return;
+        }
+        for(int i = 0; i<dist - 1; i++){
+            car = car.next;
+        }
+        car.type = this.type;
+        car.speed = dist;
+        this.type = 0;
+        this.speed = 0;
+    }
+    public void clicked() {
+        if(this.type != 5){
+            this.type = (this.type + 1)%4;
+            this.speed = 1 + 2*this.type;
+        }
+    }
+    /*
+    public void clicked() {
+        this.type = 0;
+    }*/
+    public void clear() {
+        this.type = 0;
+        this.speed = 0;
+    }
+}
+
